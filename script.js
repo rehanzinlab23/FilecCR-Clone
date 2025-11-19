c2c162930f62f4648bffc3c082f897e65ece70e8
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-11-19 18:15:32 +0500

    adding modal

---
+++ script.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector('.modal-wrap');
+const modalTriggers = document.querySelectorAll('.menu-modal');
+const modalClose = document.querySelector('.modal-close');
+modalTriggers.forEach(trigger => {
+     trigger.addEventListener('click', (e) => {
+          e.preventDefault();
+          modal.style.display = 'block';
+          document.body.style.overflow = 'hidden';
+     });
+});
+
+modalClose.addEventListener('click', () => {
+     modal.style.display = 'none';
+});
